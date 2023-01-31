0d54f0cfd310a41a49dc225a2a84efb3f1bf580c
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2023-01-30 21:44:57 -0300

    feat: criando components header e logo

--- src/components/HeaderNav/HeaderNav.tsx
+++ src/components/HeaderNav/HeaderNav.tsx
@@ -3,6 +3,7 @@ export const HeaderNav = () => {
         <div className="bg-zinc-200">
             <h1 className="text-5xl font-bold underline ">
                 Hello world!
+                Hello world!
             </h1>
         </div>
     )
